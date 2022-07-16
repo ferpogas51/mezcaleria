a58605b560c341c96a74633d81f15cce0c58b1f6
Fernando Porras <fernandoporras_2016@outlook.es>
2022-07-16 07:46:20 -0500

    sitio web terminado

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 let navbar = document.querySelector('.navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu__btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
-let searchForm = document.querySelector('.search-form');
+let searchForm = document.querySelector('.search__form');
 
-document.querySelector('#search-btn').onclick = () =>{
+document.querySelector('#search__btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
 
-let cartItem = document.querySelector('.cart-items-container');
+let cartItem = document.querySelector('.cart__items__container');
 
-document.querySelector('#cart-btn').onclick = () =>{
+document.querySelector('#cart__btn').onclick = () =>{
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
